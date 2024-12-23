5edd0ed3b1ef26f1edbb1df13f63627eaf66b454
raman0101 <ramanbhagat491@gmail.com>
2024-12-23 23:41:28 +0530

    duplicate in array using slow fast approach

---
+++ 14_duplicate.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int duplicate(vector<int> &nums){
+    int slow = nums[0];
+    int fast = nums[0];
+
+    do{
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+    }
+    while(slow != fast);
+
+    slow = nums[0];
+
+    while(slow != fast){
+        slow = nums[slow];
+        fast = nums[fast];
+    }
+    return slow;
+}
+
+int main(){
+    vector<int> nums = {1,3,4,2,2};
+
+    int ans = duplicate(nums);
+
+    cout << "The repeating element is: " << ans;
+
+    return 0;
+}
