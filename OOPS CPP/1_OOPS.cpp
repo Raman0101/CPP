02102e7c7c532ded159bad6251301bf02a2a478a
raman0101 <ramanbhagat491@gmail.com>
2025-01-18 23:13:58 +0530

    OOPS

--- OOPS CPP/1_OOPS.cpp
+++ OOPS CPP/1_OOPS.cpp
@@ -22,3 +22,31 @@ int main(){
     cout<< t1.name;
     return 0;
 }
+
+// long long minCost(vector<int>& arr, vector<int>& brr, long long k) {
+//         int n = arr.size();
+//         int m = arr.size();
+//         sort(arr.begin(), arr.end());
+//         sort(brr.begin(), brr.end());
+
+//         long long adjustmentCost = 0;
+//         for(int i=0; i<n; i++){
+//             adjustmentCost += abs(arr[i] - brr[i]);
+//         }
+//         if(n > 0){
+//             return adjustmentCost ;
+//         }
+//         else{
+//             return adjustmentCost + k;
+//         }
+        
+        
+// }
+// int main(){
+//     vector<int> arr = {-9};
+//     vector<int> brr = {9};
+//     int k = 29;
+//     int ans = minCost(arr, brr, k);
+
+//     cout << ans;
+// }
